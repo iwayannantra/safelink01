63209dfb56c3774ff4909d3a745caee855ff3e36
iwayannantra <40619001+iwayannantra@users.noreply.github.com>
2018-09-28 10:10:33 +0700

    Update random01.js

--- random01.js
+++ random01.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/06/best-practices-in-erp-software.html"}
 if(x=="2"){xxx="/2018/06/microsoft-dynamics-365-operations-erp.html"}
 if(x=="3"){xxx="/2018/06/governance-strategy-and-best-practices.html"}
 if(x=="4"){xxx="/2018/06/how-to-choose-erp-selection-whose-best.html"}
-if(x=="5"){xxx="/2018/06/aerospace-defense-for-erp.html"}a_to_ve[i].href="http://www.erp-sofware.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/06/aerospace-defense-for-erp.html"}a_to_ve[i].href="http://www.erp-software-selection.website"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
